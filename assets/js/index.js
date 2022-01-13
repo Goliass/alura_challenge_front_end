382fa0ed8c253c36c04a45a91696017474eb38d3
Elías Sauthier <elias.sauthier@compasso.com.br>
2022-01-12 23:39:26 -0300

    When pressing the 'Visualizar com o highlight' button, highlight (with highlight.js) the code informed in the editor, according to the syntax of the language selected in the respective field

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
 document.querySelector("#elEditorMenuOption").classList.add("active");
+
+const elCodeWrapper = document.querySelector("#codeWrapper");
+const elBtnVisualizeCode = document.querySelector("#btnVisualizeCode");
+const elLanguage = document.querySelector("#language");
+
+function applyHighlight() {
+  const code = elCodeWrapper.innerText; // to convert possible line breaks (<br>) etc in \n
+
+  elCodeWrapper.innerHTML = `<code class="code__text hljs ${elLanguage.value}" id="code" contenteditable="true" aria-label="Edição do código">${code}</code>`;
+
+  const elCode = document.querySelector("#code");
+  /* when using the contenteditable attribute, the browsers consider different
+    representations for line breaks (tag <br>, \n). 
+    To avoid XSS attacks, the highlight.js removes what is not text, eg. <br>
+    textContent is used to preserve the line breaks with \n (innerText would convert the \n's back to <br>).
+  */
+  elCode.textContent = code;
+
+  hljs.highlightElement(elCode);
+}
+
+elBtnVisualizeCode.addEventListener('click', applyHighlight);
